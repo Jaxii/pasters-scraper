ebed07d0338dd82fcc4cf50c40917424800c2181
Jaxii <jaxmontelongo@gmail.com>
2022-06-09 20:44:13 -0500

    added basic proxy support (with 3 random proxies from https://hidemy.name/en/proxy-list/)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use clap::{App, Arg, SubCommand};
-use linkify::{Link, LinkFinder, LinkKind};
-use reqwest::{Client, StatusCode};
+use std::{env, sync::Arc, time::Duration};
 use std::borrow::Borrow;
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::{Error, Write};
 use std::path::Path;
-use std::{env, sync::Arc, time::Duration};
+
+use clap::{App, Arg, SubCommand};
+use linkify::{Link, LinkFinder, LinkKind};
+use reqwest::{Client, ClientBuilder, StatusCode};
 use tokio::io::AsyncWriteExt;
 use tokio::time::sleep;
 use url::Url;
 
 #[tokio::main]
 async fn main() {
-
+    let client = reqwest::Client::builder();
     File::create("urls.txt").unwrap();
-    let client = Client::new();
 
     gen_url(client).await;
 }
@@ -36,14 +36,19 @@ async fn parse_and_write(data: String) {
     }
 }
 
-async fn gen_url(client: Client) {
+async fn gen_url(client: ClientBuilder) {
     const STRING: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     let mut num = 0;
+    let client2 = client
+        .proxy(reqwest::Proxy::http("https://203.30.189.196:80").unwrap())
+        .proxy(reqwest::Proxy::http("https://203.28.9.142:80").unwrap())
+        .proxy(reqwest::Proxy::http("https://45.12.31.171").unwrap())
+        .build().expect("TODO: panic message");
     for aa in STRING.chars() {
         for bb in STRING.chars() {
             for cc in STRING.chars() {
-                let composed_str = format!("https://paste.rs/{}{}{}", aa, bb, cc);
-                let response = client.get(composed_str).send().await.unwrap();
+                let composed_str = format!("https://paste.rs/{}{}{}", cc, bb, aa);
+                let response = client2.get(composed_str).send().await.unwrap();
                 if response.status() == StatusCode::OK {
                     let a = response.text().await.ok().expect("wtf?");
 
@@ -51,10 +56,9 @@ async fn gen_url(client: Client) {
 
                     num += 1;
                     println!("Count: {}/238,328", num);
-                } else if response.status().as_str() != "404"{
+                } else if response.status().as_str() != "404" {
                     println!("sleeping");
                     sleep(Duration::from_secs(30)).await;
-
                 }
 
                 // println!("{}", parsed.unwrap());
