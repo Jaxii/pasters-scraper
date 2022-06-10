b0052c7ff792493bad02577b8e7a34f7260e486b
Jaxii <jaxmontelongo@gmail.com>
2022-06-09 21:09:02 -0500

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,38 @@ use url::Url;
 
 #[tokio::main]
 async fn main() {
-    let client = reqwest::Client::builder();
-    File::create("urls.txt").unwrap();
 
-    gen_url(client).await;
+    const STRING: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    let client = reqwest::Client::builder()
+        .proxy(reqwest::Proxy::http("http://203.30.189.196:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://203.28.9.142:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://45.12.31.171:80").unwrap()).build().expect("error?");
+
+    File::create("urls.txt").unwrap();
+    let mut num = 0;
+    for aa in STRING.chars() {
+        for bb in STRING.chars() {
+            for cc in STRING.chars() {
+                do_thing(format!("https://paste.rs/{}{}{}", cc, bb, aa),&client).await;
+                num+=1;
+                println!("{} / 238328", num);
+            }
+        }
+    }
+
+
 }
 
-async fn get_html() -> Result<(String), Box<dyn std::error::Error>> {
-    let html = reqwest::get("https://paste.rs/Aaa").await?.text().await?;
-    Ok((html))
+async fn do_thing(input: String, requester: &Client) {
+    let response = requester.get(input).send().await.unwrap();
+    if response.status() == StatusCode::OK {
+        let a = response.text().await.ok().expect("wtf?");
+        parse_and_write(a).await;
+        sleep(Duration::from_millis(150)).await;
+    } else if response.status().as_str() != "404" {
+        println!("sleeping");
+        sleep(Duration::from_secs(30)).await;
+    }
 }
 
 async fn parse_and_write(data: String) {
@@ -33,36 +56,5 @@ async fn parse_and_write(data: String) {
         let mut dat = i.as_str().as_bytes().to_vec();
         dat.push(0x0au8);
         file.write_all(&*dat).expect("TODO: panic message");
-    }
-}
-
-async fn gen_url(client: ClientBuilder) {
-    const STRING: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    let mut num = 0;
-    let client2 = client
-        .proxy(reqwest::Proxy::http("https://203.30.189.196:80").unwrap())
-        .proxy(reqwest::Proxy::http("https://203.28.9.142:80").unwrap())
-        .proxy(reqwest::Proxy::http("https://45.12.31.171").unwrap())
-        .build().expect("TODO: panic message");
-    for aa in STRING.chars() {
-        for bb in STRING.chars() {
-            for cc in STRING.chars() {
-                let composed_str = format!("https://paste.rs/{}{}{}", cc, bb, aa);
-                let response = client2.get(composed_str).send().await.unwrap();
-                if response.status() == StatusCode::OK {
-                    let a = response.text().await.ok().expect("wtf?");
-
-                    parse_and_write(a).await;
-
-                    num += 1;
-                    println!("Count: {}/238,328", num);
-                } else if response.status().as_str() != "404" {
-                    println!("sleeping");
-                    sleep(Duration::from_secs(30)).await;
-                }
-
-                // println!("{}", parsed.unwrap());
-            }
-        }
     }
 }
