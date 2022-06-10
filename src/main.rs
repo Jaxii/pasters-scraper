38e77af06f606b4fc270c7a761cad1a70861e189
Jaxii <jaxmontelongo@gmail.com>
2022-06-09 22:56:09 -0500

    small changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{env, sync::Arc, time::Duration};
 use std::borrow::Borrow;
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::{Error, Write};
 use std::path::Path;
+use std::{env, sync::Arc, time::Duration};
 
 use clap::{App, Arg, SubCommand};
 use linkify::{Link, LinkFinder, LinkKind};
@@ -14,26 +14,30 @@ use url::Url;
 
 #[tokio::main]
 async fn main() {
-
     const STRING: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     let client = reqwest::Client::builder()
-        .proxy(reqwest::Proxy::http("http://203.30.189.196:80").unwrap())
-        .proxy(reqwest::Proxy::http("http://203.28.9.142:80").unwrap())
-        .proxy(reqwest::Proxy::http("http://45.12.31.171:80").unwrap()).build().expect("error?");
+        .proxy(reqwest::Proxy::http("http://23.227.38.199:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://45.12.31.152:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://203.24.103.37:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://23.227.38.79:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://203.24.103.96:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://172.67.3.71:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://23.227.38.213:80").unwrap())
+        .proxy(reqwest::Proxy::http("http://203.30.190.147:80").unwrap())
+        .build()
+        .expect("error?");
 
     File::create("urls.txt").unwrap();
     let mut num = 0;
     for aa in STRING.chars() {
         for bb in STRING.chars() {
             for cc in STRING.chars() {
-                do_thing(format!("https://paste.rs/{}{}{}", cc, bb, aa),&client).await;
-                num+=1;
+                do_thing(format!("https://paste.rs/{}{}{}", cc, bb, aa), &client).await;
+                num += 1;
                 println!("{} / 238328", num);
             }
         }
     }
-
-
 }
 
 async fn do_thing(input: String, requester: &Client) {
@@ -56,7 +60,7 @@ async fn parse_and_write(data: String) {
     let mut file = File::options().append(true).open("urls.txt").unwrap();
     for i in links {
         let mut dat = i.as_str().as_bytes().to_vec();
-        dat.push(0x0au8); // new line character in bytes
+        dat.push(b'\n');
         file.write_all(&*dat).expect("TODO: panic message");
     }
 }
